c4eaba26b852dd0b9b57395d67919b1368feb28e
filip <filip.szczesniak@spoko.pl>
2026-03-19 17:40:33 +0100

    Wykonanie zadań z Lab3

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: Text("Hello")
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("KrakFlow"),
+        ),
+        body: Center(child: Column(children: [Text("KrakFlow"),
+          Text("Organizacja studiów"),
+          Text("Dzisiejsze zadania")]),),
+      ),
     );
   }
 }
